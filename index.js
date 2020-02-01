7def56c2178795f71bc339e179e6446158db43d3
Dane David <dane@surveysparrow.com>
2020-02-01 20:52:30 +0530

    Add updater argument

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 import { useReducer } from 'react';
 
 const PATCH = '@action_types/PATCH';
+const DERIVE = '@action_types/DERIVE';
 
 const reducer = (state, action) => {
-  if ( action.type === PATCH ) {
-    return {
-      ...state,
-      ...action.payload,
-    };
+  switch ( action.type ) {
+    case PATCH:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    case DERIVE:
+      return {
+        ...state,
+        ...action.updater(state),
+      };
+    default: console.error(`Unexpected action type: ${action.type}`); return state;
   }
 };
 
 const useSetState = (initState) => {
   const [_state, _dispatch] = useReducer(reducer, initState);
   const _patchState = update => _dispatch({ type: PATCH, payload: update });
+  const _deriveState = updater => _dispatch({ type: DERIVE, updater });
 
-  const setState = (update) => {
-    const newState = {
-      ..._state,
-      ...update,
-    };
-
-    _patchState(newState);
+  const setState = (arg) => {
+    if ( typeof arg === 'function' ) {
+      _deriveState(arg);
+    } else {
+      _patchState(arg);
+    }
   };
 
   return [_state, setState];
