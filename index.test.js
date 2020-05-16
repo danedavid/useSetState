0a7b51ed738d03cc4e10d2d5897765a1c1feaa1c
Dane David <dane@surveysparrow.com>
2020-05-16 20:37:02 +0530

    Add tests for single state key

--- index.test.js
+++ index.test.js
@@ -1,17 +1,55 @@
+import React from 'react';
 import useSetState from './index';
-import Enzyme from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test('Return values', () => {
+describe('useSetState', () => {
   const App = () => {
-    const initState = { error: false };
+    const initState = { count: 0 };
     const [state, setState] = useSetState(initState);
 
-    expect(state).toBe(initState);
-    expect(typeof setState).toBe('function');
-
-    return null;
+    return (
+      <div>
+        <div id="count">{state.count}</div>
+        <button
+          id="set"
+          onClick={() => setState({ count: 1 })}
+        />
+        <button
+          id="reset"
+          onClick={() => setState({ count: 0 })}
+        />
+        <button
+          id="increment"
+          onClick={() => setState(prev => ({ ...prev, count: prev.count + 1 }))}
+        />
+      </div>
+    );
   }
+
+  test('initial state', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('#count').text()).toBe('0');
+  });
+
+  test('update state', () => {
+    const wrapper = shallow(<App/>);
+    const setButton = wrapper.find('#set');
+    const resetButton = wrapper.find('#reset');
+    setButton.simulate('click');
+    expect(wrapper.find('#count').text()).toBe('1');
+    resetButton.simulate('click');
+    expect(wrapper.find('#count').text()).toBe('0');
+  });
+
+  test('update based on prev state', () => {
+    const wrapper = shallow(<App/>);
+    const incButton = wrapper.find('#increment');
+    incButton.simulate('click');
+    expect(wrapper.find('#count').text()).toBe('1');
+    incButton.simulate('click');
+    expect(wrapper.find('#count').text()).toBe('2');
+  });
 });
