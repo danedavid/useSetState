a1cc100b744287e0deb3a21240cfc4f99e6b511d
Dane David <dane@surveysparrow.com>
2020-05-16 20:44:48 +0530

    Add tests for multiple state values

--- index.test.js
+++ index.test.js
@@ -5,14 +5,29 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const ENGLISH_GREETING = 'Hello World';
+const SPANISH_GREETING = 'Hola Mundo';
+
 describe('useSetState', () => {
   const App = () => {
-    const initState = { count: 0 };
+    const initState = {
+      count: 0,
+      greeting: ENGLISH_GREETING,
+    };
     const [state, setState] = useSetState(initState);
 
     return (
       <div>
         <div id="count">{state.count}</div>
+        <div id="greeting">{state.greeting}</div>
+        <button
+          id="language"
+          onClick={() => setState(prev => ({
+            greeting: prev.greeting === ENGLISH_GREETING
+              ? SPANISH_GREETING
+              : ENGLISH_GREETING
+          }))}
+        />
         <button
           id="set"
           onClick={() => setState({ count: 1 })}
@@ -38,8 +53,10 @@ describe('useSetState', () => {
     const wrapper = shallow(<App/>);
     const setButton = wrapper.find('#set');
     const resetButton = wrapper.find('#reset');
+
     setButton.simulate('click');
     expect(wrapper.find('#count').text()).toBe('1');
+
     resetButton.simulate('click');
     expect(wrapper.find('#count').text()).toBe('0');
   });
@@ -47,9 +64,33 @@ describe('useSetState', () => {
   test('update based on prev state', () => {
     const wrapper = shallow(<App/>);
     const incButton = wrapper.find('#increment');
+    const langButton = wrapper.find('#language');
+
     incButton.simulate('click');
     expect(wrapper.find('#count').text()).toBe('1');
+
     incButton.simulate('click');
     expect(wrapper.find('#count').text()).toBe('2');
+
+    langButton.simulate('click');
+    expect(wrapper.find('#greeting').text()).toBe(SPANISH_GREETING);
   });
+
+  test('should not alter other state values', () => {
+    const wrapper = shallow(<App/>);
+    const incButton = wrapper.find('#increment');
+    const langButton = wrapper.find('#language');
+
+    incButton.simulate('click');
+    expect(wrapper.find('#count').text()).toBe('1');
+    expect(wrapper.find('#greeting').text()).toBe(ENGLISH_GREETING);
+
+    incButton.simulate('click');
+    expect(wrapper.find('#count').text()).toBe('2');
+    expect(wrapper.find('#greeting').text()).toBe(ENGLISH_GREETING);
+
+    langButton.simulate('click');
+    expect(wrapper.find('#count').text()).toBe('2');
+    expect(wrapper.find('#greeting').text()).toBe(SPANISH_GREETING);
+  })
 });
